32a9bf2b05028494c99f0bf25d0ad6ae4d4af87d
Guido Matos Camones <guido.matos.88@gmail.com>
2020-07-12 18:18:42 -0500

    Uso de navegación con el router

---
+++ src/components/dishes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Dishes extends Component
+{
+    render() {
+        return (
+            <div>
+                <h1>Platillos</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Dishes;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import Router from './components/router';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
