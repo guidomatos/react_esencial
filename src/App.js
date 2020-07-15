0f1873f10f49c3b5c0a8f59d62b828d96fc9fc64
Guido Matos Camones <guido.matos.88@gmail.com>
2020-07-14 19:26:08 -0500

    Navegar entre rutas con React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/header';
 import Dish from './components/dish';
 import NewDish from './components/newDish';
+import Button from '@material-ui/core/Button';
 
 import './styles/App.css';
 import './styles/dish.css';
@@ -10,12 +11,20 @@ class App extends Component {
 
   dish = "tacos";
 
+  showDishes = e => {
+    e.preventDefault();
+    this.props.history.push("/platillos");
+  };
+
   render() {
     return (
       <div className="App">
           <Header/>
           <NewDish></NewDish>
-          <Dish name={this.dish} qty="3" ></Dish>
+          {/* <Dish name={this.dish} qty="3" ></Dish> */}
+          <Button variant="contained" color="secondary" onClick={this.showDishes}>
+            Elegir
+          </Button>
       </div>
     );
   }
